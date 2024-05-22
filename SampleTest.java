8717087c80805de5287f24fbb8b9bad00fe99ea2
KHOLUD-2030 <162402488+KHOLUD-2030@users.noreply.github.com>
2024-05-22 18:32:22 +1000

    Create SampleTest.java

---
+++ SampleTest.java
@@ -0,0 +1,15 @@
+// SampleTest.java
+package junitpipeline;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void simpleTest() {
+        int a = 1;
+        int b = 2;
+        assertTrue(a + b == 3);
+    }
+}
